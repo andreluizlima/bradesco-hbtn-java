ddcc07c46b4dd0edf3fd0df25995ed0562cc9250
André Luiz Lima <andre.luiz.lima.1997@gmail.com>
2025-07-15 15:45:55 -0300

    Status

---
+++ Classes_Objetos/status/PersonagemGame.java
@@ -0,0 +1,36 @@
+
+public class PersonagemGame {
+	private int saudeAtual;
+	private String nome;
+	private String status;
+
+	public int getSaudeAtual(){
+		return saudeAtual;
+	}
+
+	public void setSaudeAtual(int saudeAtual){
+		status = saudeAtual > 0 ? "vivo" : "morto";
+		this.saudeAtual = saudeAtual;
+	}
+
+	public String getNome(){
+		return nome;
+	}
+
+	public void setNome(String nome){
+		this.nome = nome;
+	}
+
+	public String getStatus(){
+		return status;
+	}
+
+
+	public void tomarDano(int quantidadeDeDano) {
+		setSaudeAtual(saudeAtual - quantidadeDeDano  <= 0 ? 0 : saudeAtual - quantidadeDeDano);
+	}
+
+	public void receberCura(int quantidadeDeCura) {
+		setSaudeAtual(saudeAtual + quantidadeDeCura  >= 100 ? 100 : saudeAtual + quantidadeDeCura);
+	}
+}
