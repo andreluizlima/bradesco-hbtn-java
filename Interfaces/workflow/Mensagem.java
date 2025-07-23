5fbb0f525800fef3ee02246756d15b5456965f63
André Luiz Lima <andre.luiz.lima.1997@gmail.com>
2025-07-23 15:26:57 -0300

    Interfaces

---
+++ Interfaces/workflow/Mensagem.java
@@ -0,0 +1,20 @@
+public class Mensagem {
+    private String texto;
+    private TipoMensagem tipo;
+
+    
+    public String getTexto() {
+        return texto;
+    }
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+    public TipoMensagem getTipo() {
+        return tipo;
+    }
+    public void setTipo(TipoMensagem tipo) {
+        this.tipo = tipo;
+    }
+
+    
+}
