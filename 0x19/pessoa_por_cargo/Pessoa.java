b1b25b681d68b46561a56b58074b9be53c28154d
André Luiz Lima <andre.luiz.lima.1997@gmail.com>
2025-09-15 11:19:46 -0300

    fix dot

--- 0x19/pessoa_por_cargo/Pessoa.java
+++ 0x19/pessoa_por_cargo/Pessoa.java
@@ -1,3 +1,5 @@
+import java.util.Locale;
+
 public class Pessoa {
     private int codigo;
     private String nome;
@@ -35,6 +37,6 @@ public class Pessoa {
 
     @Override
     public String toString() {
-        return String.format("[%d] %s %s %d R$ %f", codigo, nome, cargo, idade, salario);
+        return String.format(Locale.US,"[%d] %s %s %d R$ %f", codigo, nome, cargo, idade, salario);
     }
 }
